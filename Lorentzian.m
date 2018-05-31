1f00ae52fb12dd18f834019baf454589c285a2ee
Fittzzzzz <33721718+Fittzzzzz@users.noreply.github.com>
2018-05-31 14:01:17 +0800

    Update Lorentzian.m

--- Lorentzian.m
+++ Lorentzian.m
@@ -1,17 +1,17 @@
 function [ya] = Lorentzian()
-ak = 5;
+ak = 500;
 fk = 654;
 N = 2249; 
 f = 250:N;
 dk = 10; 
 y1 = ak * 1./(1.+((fk-f)./(dk/2)).^2);
-ak = 10;
+ak = 1000;
 fk = 1002;
 N = 2249 
 f = 250:N;
 dk = 20;
 y2 = ak * 1./(1.+((fk-f)./(dk/2)).^2);
-ak = 15;
+ak = 1500;
 fk = 1455;
 N = 2249; 
 f = 250:N;
@@ -21,7 +21,7 @@ x = y1+y2+y3;
 h = awgn(x,5,10)
 y=2500./f+4
 b=250:1:2249
-ya=x
+ya=h
 %plot(ya);
 %xlswrite('C:\Users\fangzheng\Desktop\Lorentzian1.xlsx',x.')
 end
